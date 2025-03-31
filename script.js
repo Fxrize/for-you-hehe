a1672d444578a7394b525c0ff9729dc61d199577
yann u r <156898523+Fxrize@users.noreply.github.com>
2025-03-31 16:58:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
 
  let p = document.createElement("p");
- p.innerText = "Yeayyy, kamu membuat keputusan yang tepat!!! 🎉 \nI love you too <3";
+ p.innerText = "Yeayyy, kamu membuat keputusan yang tepat!!!🎉 \nI love you too <3";
   p.style.fontSize = "2rem";
   p.style.fontWeight = "bold";
   p.style.textAlign = "center";
